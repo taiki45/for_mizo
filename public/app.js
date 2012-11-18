3f55051d6086bb6d5e8e82041303eaea6ce453d8
Taiki ONO <taiks.4559@gmail.com>
2012-11-18 15:43:29 +0900

    add enabling others func

--- public/app.js
+++ public/app.js
@@ -8,24 +8,13 @@
             elem.pickChecked = function(){
                 return this.filter(':checked');
             };
-            elem.disableRest = function(case_num){
-                switch(case_num){
-                    case "1":
-                        this.filter(function(){
-                            return $(this).val() == "1"
-                        }).attr("disabled", "disabled");
-                        break;
-                    case "2":
-                        this.filter(function(){
-                            return $(this).val() == "2"
-                        }).attr("disabled", "disabled");
-                        break;
-                    case "3":
-                        this.filter(function(){
-                            return $(this).val() == "3"
-                        }).attr("disabled", "disabled");
-                        break;
-                };
+            elem.disableRest = function(num){
+                this.filter(function(index, elem){
+                    return jQuery(elem).val() == num;
+                }).attr("disabled", true);
+                this.filter(function(index, elem){
+                    return jQuery(elem).val() != num;
+                }).attr("disabled", false);
             };
         });
         radios.forEach(function(elem){
