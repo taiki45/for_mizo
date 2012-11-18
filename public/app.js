5ee71a96a955ab5c4389f0ec14e73fcdf5448e37
Taiki ONO <taiks.4559@gmail.com>
2012-11-18 15:19:02 +0900

    works with some bug

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 (function(){
     jQuery(function(){
-        var group_a = jQuery(':radio[name="one"]');
-        var group_b = jQuery(':radio[name="two"]');
-        var group_c = jQuery(':radio[name="three"]');
+        var group_a = jQuery(':radio[name="a"]');
+        var group_b = jQuery(':radio[name="b"]');
+        var group_c = jQuery(':radio[name="c"]');
         var radios = [group_a, group_b, group_c];
         radios.forEach(function(elem){
             elem.pickChecked = function(){
@@ -10,31 +10,33 @@
             };
             elem.disableRest = function(case_num){
                 switch(case_num){
-                    case '1':
-                        $(this).filter(function(){
-                            this.val() == '1'
+                    case "1":
+                        this.filter(function(){
+                            return $(this).val() == "1"
                         }).attr("disabled", "disabled");
                         break;
-                    case '2':
-                        $(this).filter(function(){
-                            this.val() == '2'
+                    case "2":
+                        this.filter(function(){
+                            return $(this).val() == "2"
                         }).attr("disabled", "disabled");
                         break;
-                    case '3':
-                        $(this).filter(function(){
-                            this.val() == '3'
+                    case "3":
+                        this.filter(function(){
+                            return $(this).val() == "3"
                         }).attr("disabled", "disabled");
                         break;
                 };
             };
         });
-        $(radios).change(function(){
-            if(group_a.is(':checked')){
-                group_b.disableRest(group_a.pickChecked());
-                if(group_b.is(':checked')){
-                    group_c.disableRest(group_b.pickChecked());
+        radios.forEach(function(elem){
+            elem.change(function(){
+                if(group_a.is(':checked')){
+                    group_b.disableRest(group_a.pickChecked().val());
+                    if(group_b.is(':checked')){
+                        group_c.disableRest(group_b.pickChecked().val());
+                    };
                 };
-            };
+            });
         });
     });
 }());
